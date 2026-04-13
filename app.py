193a46b8a486965872a5fe6bbe3863a11555b6bf
Narayan Kishor Adhude <narayanadhude2004@gmail.com>
2026-04-13 16:40:07 +0530

    Updated

--- app.py
+++ app.py
@@ -179,7 +179,8 @@ def login():
 @app.route('/home')
 def home_page():
     return render_template('home.html')
+import os
 
 if __name__ == "__main__":
-    app.run(debug=True)
-    
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host="0.0.0.0", port=port)
